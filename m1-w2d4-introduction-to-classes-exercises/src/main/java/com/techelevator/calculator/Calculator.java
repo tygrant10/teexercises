0946bbc12b80571311c5e4e85bf8be9e431b87e6
Tyler Grant <tygrant10@gmail.com>
2018-01-29 08:46:06 -0500

    w2d4

--- m1-w2d4-introduction-to-classes-exercises/src/main/java/com/techelevator/calculator/Calculator.java
+++ m1-w2d4-introduction-to-classes-exercises/src/main/java/com/techelevator/calculator/Calculator.java
@@ -2,8 +2,34 @@ package com.techelevator.calculator;
 
 public class Calculator {
 
-	//
-	// Write code here
-	//
+	private int currentValue;
 	
+	
+	public int add(int addend) {
+		
+		return currentValue;
+	}
+	
+	public int multiply(int multiplier) {
+		return currentValue;
+	}
+	   
+	
+	public int subtract(int subtrahend) {
+		return currentValue;
+	}
+	   
+	
+	public int power(int exponent) {
+		return currentValue;
+	}
+	   
+	
+	public void reset() {
+		currentValue = 0;
+	}
+
+	public int getCurrentValue() {
+		return currentValue;
+	}
 }
