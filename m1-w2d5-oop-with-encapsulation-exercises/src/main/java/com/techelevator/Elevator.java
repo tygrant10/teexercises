7e39a1e6f52cb24b539c1ff551eabef9fb624570
Tyler Grant <tygrant10@gmail.com>
2018-01-31 23:04:10 -0500

    w2d5

--- m1-w2d5-oop-with-encapsulation-exercises/src/main/java/com/techelevator/Elevator.java
+++ m1-w2d5-oop-with-encapsulation-exercises/src/main/java/com/techelevator/Elevator.java
@@ -1,5 +1,49 @@
 package com.techelevator;
 
 public class Elevator {
+	private int currentFloor = 1;
+	private int numberOfFloors;
+	private boolean doorOpen;
+
+	Elevator(int totalNumberOffloors) {
+		this.numberOfFloors = totalNumberOffloors;
+	}
+
+	void openDoor() {
+		doorOpen = true;
+	}
+
+	void closeDoor() {
+		doorOpen = false;
+	}
+
+	public void goUp(int desiredFloor) {
+		if (!doorOpen) {
+			if (getNumberOfFloors() >= desiredFloor)
+				currentFloor += desiredFloor;
+		}
+		currentFloor = getCurrentFloor();
+	}
+
+	public void goDown(int desiredFloor) {
+		if (!doorOpen) {
+			if (desiredFloor >= 1) {
+				currentFloor -= desiredFloor;
+			}
+		}
+		currentFloor = getCurrentFloor();
+	}
+
+	public int getCurrentFloor() {
+		return currentFloor;
+	}
+
+	public int getNumberOfFloors() {
+		return numberOfFloors;
+	}
+
+	public boolean isDoorOpen() {
+		return doorOpen;
+	}
 
 }
