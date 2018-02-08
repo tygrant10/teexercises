e9cf9f9ef10ce6d692ffec66c9fdb53ebac1beb9
Tyler Grant <tygrant10@gmail.com>
2018-02-07 19:00:08 -0500

    updatew4d3

--- m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FileSplitter.java
+++ m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FileSplitter.java
@@ -2,31 +2,42 @@ package com.techelevator;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FileSplitter {
 
 	public static void main(String[] args) {
-		File originalFile = new File("/Users/tylergrant/Desktop/FizzBuzz.txt");
+		//File originalFile = new File("/Users/tylergrant/Desktop/FizzBuzz.txt");
+		File originalFile = new File("alices_adventures_in_wonderland.txt");
 		
+		int counter = 0;
 
 			try (Scanner lineReader = new Scanner(originalFile)) {
 				
 					while (lineReader.hasNextLine()) {
-						int counter = 1;
+						
+						counter++;
 						File newFile = new File("/Users/tylergrant/Desktop/piece" + Integer.toString(counter));
-						PrintWriter writer = new PrintWriter(newFile);
-					
+						newFile.createNewFile();
+						try (PrintWriter writer = new PrintWriter(newFile)) {
+							System.out.println(counter);
 						
 					for(int i = 0; i < 100; i++) {
+						if(lineReader.hasNextLine()) {
 						String line = lineReader.nextLine();
 						writer.println(line);
+						}
+					}
 					
 					}
 					}
 
 	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
