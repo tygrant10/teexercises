c6aaf4b97879bfa132e0c81d7cefa0ecad11bf95
Tyler Grant <tygrant10@gmail.com>
2018-03-10 20:34:01 -0500

    updates

--- m3-w8d2-controllers_part2_exercises/src/main/java/com/techelevator/HelloController.java
+++ m3-w8d2-controllers_part2_exercises/src/main/java/com/techelevator/HelloController.java
@@ -2,12 +2,18 @@ package com.techelevator;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.techelevator.model.ReviewDao;
+
 @Controller 
 public class HelloController {
+	
+	@Autowired
+	ReviewDao reviewDao;
 
 	@RequestMapping("/greeting")
 	public String displayGreeting() {
